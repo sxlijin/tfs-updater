9b786db08e27233eae228cc63b1827a96a8a98f3
Samuel Lijin <sxlijin+github@gmail.com>
2016-04-05 01:00:59 -0500

    refactored DOUBLE-CLICK-ME... to recognize deprecation of .bat and .sh scripts

--- DOUBLE-CLICK-ME-TO-GENERATE-UPDATED-HISCORES.py
+++ DOUBLE-CLICK-ME-TO-GENERATE-UPDATED-HISCORES.py
@@ -15,20 +15,34 @@
 ##   You should have received a copy of the GNU General Public License
 ##   along with TFS HISCORES UPDATER. If not, see http://www.gnu.org/licenses/.
 
-import subprocess
-from time import sleep
-from sys import platform
 
-exe = './src/autoupdate.sh' ## Default
-if platform == 'win32': exe = r'src\autoupdate.bat'
-elif platform in ('linux2','darwin'): exe = './src/autoupdate.sh'
-else:
-  print 'Your operating system was not recognized. Defaulting to autoupdate.sh.'
-  print 'Please run update.py (in src) to update the hiscores instead.'
+
+## CHANGE THIS LINE IF THE GDOC FOR THE HISCORES IS CHANGED
+gdoc_url_key = '0Arkz2dsnDdvVdG1DLWU4NDNwSndoS0FCb1hIekw1a2c'
+## CHANGE THIS LINE IF THE GDOC FOR THE HISCORES IS CHANGED
+
+
+
+import src.update
+##import subprocess
+#from time import sleep
+#from sys import platform
+#
+#exe = './src/autoupdate.sh' ## Default
+#if platform == 'win32': exe = r'src\autoupdate.bat'
+#elif platform in ('linux2','darwin'): exe = './src/autoupdate.sh'
+#else:
+#  print 'Your operating system was not recognized. Defaulting to autoupdate.sh.'
+#  print 'Please run update.py (in src) to update the hiscores instead.'
 
 print 'Please wait... calling the updater.'
 print 'The update log will be recorded in the most recent file in log/.'
 
 sleep(5) #Give user time to read this.
 
-subprocess.call(exe)
+src.update.auto_update()
+
+raw_input("Finished generating updated-hiscores.csv. Press any key to finish.")
+sys.exit(0)
+
+#subprocess.call(exe)
